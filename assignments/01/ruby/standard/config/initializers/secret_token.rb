209c10e50eb1cc0f3943ea6eb2f276bdaecd9d7c
Kip Landergren <k.landergren@gmail.com>
2011-06-06 13:23:06 -0400

    added assignment 01; created standard rails app "standard" with examples in public/index.html

---
+++ assignments/01/ruby/standard/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Standard::Application.config.secret_token = 'd82c1ce1edf314587e03fecc8bbbfbb915e932eca6e9b4407070a79b7937cffccaca3cde6322f79aca71c9590bfeea45b5c55686387d16c5a5d769a39c9c2369'
