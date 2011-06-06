0af59895d165a9ce3d7bc6bde2eb816f34889bd6
Sunny Gleason <sunny.gleason@gmail.com>
2011-06-06 07:06:09 -0400

    adding more code listings for theory lecture 2

--- lectures/week01/scheme/w01l02.scm
+++ lectures/week01/scheme/w01l02.scm
@@ -89,3 +89,83 @@
 ;; print awesome message 10 times for 'joe user'
 (awesome "joe user" 10)
 
+;;
+;; iteration of a 2-dimensional space: driver function
+;;
+(define (f x y) (f-iter 0 x 0 y))
+
+;; helper function to output point
+(define (show i j) (write-line (string-append "(x:" (number->string i) ", y:" (number->string j) ")")))
+
+;;
+;; iteration of a 2-dimensional space: implementation
+;;
+(define (f-iter i x j y)
+  ;; no 'let' bindings, 'let' is used just for sequence
+  ;; of operations
+  (let ()
+    (if (and (< i x) (< j y)) (show i j))
+    (if (< j y)
+      (cond ((< i x) (f-iter (+ i 1) x j y))
+            ((= i x) (f-iter 0 x (+ j 1) y))))))
+
+;;
+;; fibonacci sequence: tree recursive form
+;;
+(define (fib n)
+  (cond ((= n 0) 0)) ((= n 1) 1))
+        (else (+ (fib (- n 1)) (fib (- n 2))))))
+
+;;
+;; fibonacci sequence: tail-recursive form
+;; (driver function)
+;;
+(define (fib n) (fib-iter 1 0 n))
+
+;;
+;; fibonacci sequence: tail-recursive form
+;; (implementation)
+;;
+(define (fib-iter a b count)
+  (if (= count 0) b
+    (fib-iter (+ a b) a (- count 1))))
+
+
+;;
+;; SAMPLE CODE : Infinite Game
+;;   play by invoking the function by typing '(game)'
+;;   without single quotes
+;;
+
+;; the game starts in the center of the boat
+(define (game) (center-boat))
+
+;;
+;; at the center of the boat, the user may move
+;; left or right safely
+;;
+(define (center-boat)
+  (let ()
+    (write-line "You are in the center of the boat. Move left or right?")
+    (let ((d (read-line)))
+      (cond ((string=? d "left") (side-boat d))
+            ((string=? d "right") (side-boat d))
+            (else (center-boat))))))
+
+;;
+;; at the side of the boat, the user must choose
+;; wisely
+;;
+(define (side-boat side)
+  (let ((otherside (if (string=? side "left") "right" "left")))
+    (write-line
+      (string-append "You are at the " side " of the boat. Move left or right?"))
+    (let ((d (read-line)))
+      (cond ((string=? d side) (ocean))
+            (else (side-boat otherside))))))
+
+;;
+;; 'sorry, you lose'
+;;
+(define (ocean)
+  (write-line "you fell in the ocean! Game Over."))
